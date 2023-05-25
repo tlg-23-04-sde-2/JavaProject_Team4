1f6e7463fe68b0fba0fe76f1803356075be8948e
Jennifer Fedorka <Jennifer.fedorka@tlgcohort.com>
2023-05-25 17:30:32 -0500

    Delete Start Files

--- src/com/matchgorithm/Profile.java
+++ src/com/matchgorithm/Profile.java
@@ -1,4 +1,22 @@
 package com.matchgorithm;
 
+import java.util.Random;
+
 class Profile {
+
+    //Instance Variables
+    private Picture pic;
+    private Name name;
+    private Bio bio;
+    private int age;
+
+    //Constructor
+    public Profile() {
+        pic = new Picture();
+        name = new Name();
+        bio = new Bio();
+        Random rand = new Random();
+        age = rand.nextInt(68)+ 18;
+    }
+
 }
