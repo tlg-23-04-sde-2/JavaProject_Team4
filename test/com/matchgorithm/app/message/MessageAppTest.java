c3766398e5042e42998fe59a1f615092607be710
jianxing-ma <jianxing.ma@tlgcohort.com>
2023-06-01 17:16:02 -0400

    Added MessageAppTest.java

---
+++ test/com/matchgorithm/app/message/MessageAppTest.java
@@ -0,0 +1,12 @@
+package com.matchgorithm.app.message;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class MessageAppTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+}
